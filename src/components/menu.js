c1396df27f99735ff394e5498c89c5102d73f6c0
oresFCK <kappa123.wow@gmail.com>
2023-06-27 00:46:27 +0200

    menu and router

---
+++ src/components/menu.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+
+const MenuComponent = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenuOpen}>
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+           <MenuItem onClick={handleMenuClose}>
+              <Link to="/gallery" style={{ textDecoration: 'none', color: 'inherit' }}>Galeria</Link>
+            </MenuItem>
+            <MenuItem onClick={handleMenuClose}>
+              <Link to="/news" style={{ textDecoration: 'none', color: 'inherit' }}>Aktulności</Link>
+            </MenuItem>
+          </Menu>
+          <Typography variant="h6">Menu</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default MenuComponent;
---
+++ src/components/news.js
@@ -0,0 +1,10 @@
+function News() {
+    return (
+      <div>
+          <h1>huj</h1>
+      </div>
+    );
+  }
+  
+  export default News;
+  
